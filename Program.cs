27371d0cb8391eedf0d53a0ec9a06346de9280ac
Miguelsanta23 <msantafecastrillon@gmail.com>
2025-10-28 23:15:02 -0500

    Creacion proyecto
    Hice la creacion del proyecto con la configuracion inicial.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaRestaurante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de Restaurante");
+        }
+    }
+}
