59196eec8888e052ff5df70407bd86f66ee55835
Miguelsanta23 <msantafecastrillon@gmail.com>
2025-11-08 21:05:01 -0500

    Entrada del Programa
    Inicializacion de servicios y del menu principal en el program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Proyecto_Restaurante.Servicios;
+using Proyecto_Restaurante.UI;
 
-namespace SistemaRestaurante
+namespace Proyecto_Restaurante
 {
+    // Clase principal del programa
     class Program
     {
+        // Punto de entrada del programa
         static void Main(string[] args)
         {
-            Console.WriteLine("Sistema de Restaurante");
+            // Inicializar servicios
+            ServicioRestaurante servicioRestaurante = new ServicioRestaurante();
+            ServicioCliente servicioCliente = new ServicioCliente();
+            ServicioPlato servicioPlato = new ServicioPlato();
+            ServicioPedido servicioPedido = new ServicioPedido();
+
+            // Mostrar menu principal
+            MenuPrincipal menuPrincipal = new MenuPrincipal(servicioRestaurante, servicioCliente, 
+                                                            servicioPlato, servicioPedido);
+            menuPrincipal.Mostrar();
         }
     }
 }
